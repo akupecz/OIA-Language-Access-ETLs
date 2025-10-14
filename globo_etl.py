4d84b439c055598afd7fc4bda31b162ae51ece60
akupecz <anthony.kupecz@phila.gov>
2025-10-14 19:05:23 +0000

    Add sftp setup functions

--- globo_etl.py
+++ globo_etl.py
@@ -1,9 +1,15 @@
 import citygeo_secrets as cgs
-from config import SFTP_SECRET, GLOBO_DIRECTORY, ULG_DIRECTORY, DB_SECRET_LOGIN, DB_SECRET_HOST
+from config import SFTP_SECRET, GLOBO_DIRECTORY, DB_SECRET_LOGIN, DB_SECRET_HOST
 import fabric 
 from paramiko.sftp_client import SFTPClient
+import utils
 
 def main():
+    cgs.set_config(keeper_dir='~')
+    # Create sftp connection at specified directory 
+    sftp_client = utils.setup_sftp(GLOBO_DIRECTORY)
+
+
     return
 
 if __name__ == '__main__': 
